ded1779fed97fb68dd6b97fd38f007d04804de6e
Charles <wcharlesw15@gmail.com>
2021-06-06 17:44:09 -0300

    lista de perguntas

---
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -0,0 +1,52 @@
+import 'package:dev_quiz/_core/core.dart';
+import 'package:dev_quiz/challenge/widgets/answer/answer_widget.dart';
+import 'package:flutter/material.dart';
+
+class QuizWidget extends StatelessWidget {
+  final String title;
+  const QuizWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: AppTextStyles.heading,
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+            child: AnswerWidget(
+              title:
+                  "Possibilita a criação de aplicativos compilados nativamente",
+              isRight: false,
+              isSelected: true,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+            child: AnswerWidget(
+              title:
+                  "Possibilita a criação de aplicativos compilados nativamente",
+              isRight: false,
+              isSelected: true,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+            child: AnswerWidget(
+              title:
+                  "Possibilita a criação de aplicativos compilados nativamente",
+              isRight: false,
+              isSelected: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
