5cb3947c6bdf0de926e7de7842aaab6f7b394f29
Charles <wcharlesw15@gmail.com>
2021-06-08 18:48:58 -0300

    entrypoint em splash

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,10 +1,14 @@
 import 'package:dev_quiz/_core/app_images.dart';
+import 'package:dev_quiz/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:dev_quiz/_core/app_gradients.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2)).then((_) => Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => HomePage())));
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(gradient: AppGradients.linear),
